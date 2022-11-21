12cbdd8efdff04daa9bba799ea67ba46ab33d0b3
nothingrotf <enderbr4@gmail.com>
2022-11-21 19:56:07 -0300

    custom-loggin

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import { config } from "./config/config";
+import Logging from "./library/Logging";
 
 const router = express();
 
 mongoose.connect(config.mongo.url)
 	.then(startServer)
 	.catch(error => {
-		throw new Error(error);
+		Logging.error("❌ Unable to connect");
+		Logging.error(error);
 	});
 
 function startServer() {
 	router.listen(config.server.port, () => {
-		console.log(`✔ server is running on http://localhost:${ config.server.port }`);
+		Logging.info(`✔ server is running on http://localhost:${ config.server.port }`);
 	});
 }
