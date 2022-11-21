abb44867ca074376a82de828b691e10c94a3db6d
nothingrotf <enderbr4@gmail.com>
2022-11-21 19:15:10 -0300

    bugfix-db-pass

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const MONGO_USERNAME = process.env.DB_USER || "";
-const MONGO_PASSWORD = process.env.DB_PASSWORD || "";
+const MONGO_PASSWORD = process.env.DB_PASS || "";
 const MONGO_URL = `mongodb+srv://${ MONGO_USERNAME }:${ MONGO_PASSWORD }@cluster0.itdlfwk.mongodb.net/?retryWrites=true&w=majority`;
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number( process.env.SERVER_PORT ) : 3001;
